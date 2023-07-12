91a2d09529aabc416875ba52a9009f11d911fd01
jortorres <jordan.torres1@gmail.colm>
2023-07-12 02:53:07 -0500

    New file

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let person = {
+    name: 'Jordan',
+    age: 42
+};
+
+// Dot Notation
+person.name = 'John';
+person.age = "62";
+
+// Bracket Notation
+let selection = 'name'
+person[selection] = 'Mary'
+
+console.log(person.name);
