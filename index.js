254ba483d52260a59dd85fb8d067bd002a99aef3
jortorres <jordan.torres1@gmail.colm>
2023-07-14 01:26:25 -0500

    Changed the Input

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function greet(name, lastName) {
     console.log('Hello '+ name + ' ' + lastName);    
-}
+} 
 
+const name = prompt('What is first name? ')
+alert(`Your name is: ${name}`);
 
-greet('John', 'Smith');
+greet(name, 'Smith');
 // testing
-
+/// this will happen 
 
